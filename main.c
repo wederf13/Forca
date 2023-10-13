c5181f43254d25a4aa8c7873739654d0cb16d95e
Weder <147000869+wederf13@users.noreply.github.com>
2023-10-13 16:29:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 int main ()
 {
-  int i, n;
-  int f = 1;
-  scanf("%d", &n);
-  
-  for (i = n; i > 1; i--)
-  {
-      f = f * i;
-  }
-    
-  printf("%d! = %d", n, f);
+  printf("tnc");
  
   return 0;
 }
