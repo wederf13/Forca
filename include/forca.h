71536fb6b9e113e2af04b489119777320712acc6
tiago <97364045+tivgo@users.noreply.github.com>
2023-10-24 15:07:50 -0300

    Update forca.h

--- include/forca.h
+++ include/forca.h
@@ -12,16 +12,16 @@
 #include <time.h>
 
 // function declarations
-int is_word_guessed();
+int is_word_guessed(char word[], char guessed_word[]);
 
 // procedures declarations
 void read_game_data(int *theme, int *level, char name[50], char theme_name[30], char level_name[10]);
 void display_hangman(int mistakes);
 void choose_word(int level, int theme, char word[50]);
-void game();
-void show_menu();
-void show_project_info();
-void show_exit_message();
-void show_play_again_message();
+void game(char word[50], char guessed_word[50], char incorrect_letters[26], char name[50], char theme_name[30], char level_name[10]);
+void display_menu();
+void display_project_info();
+void display_exit_message();
+void display_play_again_message();
 
 #endif 
